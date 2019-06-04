7a0f45f8900170242f2e82b84709beb22786f4ff
kalkidan <kalkidanmulatu2@gmail.com>
2019-06-03 19:53:19 -0500

    eslint

--- database-mysql/db-helpers.js
+++ database-mysql/db-helpers.js
@@ -1,11 +1,12 @@
+const Sequelize = require('sequelize');
 const { Users } = require('./index');
 const { Messages } = require('./index');
-const Sequelize = require('sequelize');
 // const dummyMessages = require('../client/src/dummy-message-data');
 
 const { Op } = Sequelize;
 
-const getUsers = user => Users.findAll({ where: { id: { [Op.ne]: user.id } } }).then(results => ({ user, allUsers: results }));
+const getUsers = user => Users.findAll({ where: { id: { [Op.ne]: user.id } } })
+  .then(results => ({ user, allUsers: results }));
 
 // getUsers();
 // helper to get all message from a user based on userId
@@ -23,8 +24,8 @@ const getInbox = userId => Messages.findAll({
   },
   order: [['createdAt', 'ASC']],
 }).then(messages => messages.reduce((userIds, m) => {
-  if (m.fromId != userId && !userIds.includes(m.fromId)) userIds.push(m.fromId);
-  if (m.toId != userId && !userIds.includes(m.toId)) userIds.push(m.toId);
+  if (m.fromId !== userId && !userIds.includes(m.fromId)) userIds.push(m.fromId);
+  if (m.toId !== userId && !userIds.includes(m.toId)) userIds.push(m.toId);
   return userIds;
 }, []));
 
